84dcbeeefbbbf9d20712cbf4f8d828f9ec4764c1
andrey <andrey-ne@mail.ru>
2022-06-02 19:06:09 +0700

    add cellObject decorator

--- cell/src/decorators.ts
+++ cell/src/decorators.ts
@@ -1,17 +1,18 @@
 import {Cell, ICellOptions} from "./cell";
 import {BaseCell} from "./baseCell";
+import {ObservableObject} from "./ObservableObject";
 
 const KEY_VALUE_CELLS = Symbol('ObservableCells')
 export type CellDecorator =
     ((options: ICellOptions<any>) => (PropertyDecorator & MethodDecorator))
     & PropertyDecorator
-    & MethodDecorator
+    & MethodDecorator;
 
-const getCell = (self: { [KEY_VALUE_CELLS]?: { [key: string]: BaseCell } }, prop, descr: PropertyDescriptor, options) => {
+const getCell = <T = any>(self: { [KEY_VALUE_CELLS]?: { [key: string]: BaseCell } }, prop, descr: PropertyDescriptor, options, defaultValue = null) : BaseCell<T> => {
     if (self[KEY_VALUE_CELLS] && self[KEY_VALUE_CELLS][prop])
         return self[KEY_VALUE_CELLS][prop];
     self[KEY_VALUE_CELLS] ??= {};
-    let pull = null; // if simple observable
+    let pull = defaultValue; // if simple observable
     if (descr) {
         if (descr.get)
             pull = descr.get.bind(self); // if computed observable
@@ -21,7 +22,7 @@ const getCell = (self: { [KEY_VALUE_CELLS]?: { [key: string]: BaseCell } }, prop
     return self[KEY_VALUE_CELLS][prop] = options ? new Cell(pull, options) : new BaseCell(pull);
 }
 
-export const cell: CellDecorator = ((options: ICellOptions<any>, prop?, descr?) => {
+export const cell: CellDecorator = ((options: ICellOptions<any>, prop?, descr?, key2?) => {
     if (prop !== undefined) {
         return cell(null)(options, prop, descr);
     }
@@ -42,6 +43,28 @@ export const cell: CellDecorator = ((options: ICellOptions<any>, prop?, descr?) 
             set(value) {
                 const cell = getCell(this, prop, descr, options);
                 cell.set(value);
+            },
+            configurable: true
+        };
+        return descriptor;
+    }
+}) as CellDecorator;
+
+
+
+export const cellObject: CellDecorator = ((options: ICellOptions<any>, prop?, descr?) => {
+    if (prop !== undefined) {
+        return cellObject(null)(options, prop, descr);
+    }
+    return function cellDecorator(target, prop, descr) {
+        const descriptor = {
+            get() {
+                const cell = getCell<ObservableObject<any>>(this, prop, descr, options, new ObservableObject<any>(null));
+                return cell.get().Value;
+            },
+            set(value) {
+                const cell = getCell<ObservableObject<any>>(this, prop, descr, options, new ObservableObject<any>(null));
+                cell.get().Set(value);
             },
             configurable: true
         };
