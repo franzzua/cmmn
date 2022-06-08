8688e6f4cf1440894a931823439ca572005a779d
andrey <andrey-ne@mail.ru>
2022-06-08 11:54:54 +0700

    not-active cells works same as pull

---
+++ cell/specs/not-active.spec.ts
@@ -0,0 +1,18 @@
+import {expect, suite, test} from '@cmmn/tools/test';
+import {Actualizator} from '../src/actualizator';
+import {Cell} from '../src/cell';
+
+const noop = () => {
+};
+
+@suite
+class NotActiveSpec {
+    @test
+    getActual() {
+        const a = new Cell(7);
+        const b = new Cell(() => a.get());
+        expect(b.get()).toEqual(a.get());
+        a.set(9);
+        expect(b.get()).toEqual(a.get());
+    }
+}
--- cell/src/baseCell.ts
+++ cell/src/baseCell.ts
@@ -69,7 +69,9 @@ export class BaseCell<T = any> extends EventEmitter<{
         this.error = error;
         const oldValue = this.value;
         this.value = value;
-        this.state = CellState.Actual;
+        if (this.isActive) {
+            this.state = CellState.Actual;
+        }
         if (oldValue !== value) {
             if (oldValue instanceof EventEmitterBase) {
                 oldValue.off('change', this.onValueContentChanged);
