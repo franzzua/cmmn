77c630749302e8b10bab99fd1468e99fd02a67de
fransua <fransua.ne@gmail.com>
2023-12-18 18:02:47 +0100

    add LocalStorageCell

--- infr/index.ts
+++ infr/index.ts
@@ -3,6 +3,7 @@ import { CellQuery } from "./src/cell.query";
 import {Request, RequestFailedError, RequestAbortedError} from "./src/request";
 
 export {Api, Request, RequestFailedError, RequestAbortedError, CellQuery};
+export {LocalStorageCell, cellLS} from "./src/storage/local.storage.cell";
 export function useApi(fetch, baseUrl: string){
     Request.fetch = fetch;
     return {provide: Api, useValue: new Api().with({apiUrl: baseUrl})};
