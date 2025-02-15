8da35018f9918f8bbcc992f33d3209f69762a06d
fransua <fransua.ne@gmail.com>
2025-02-15 19:39:54 +0100

    version 3.0.0-alpha-3

---
+++ tools/commands/version.js
@@ -0,0 +1,17 @@
+import {Target} from "../helpers/target.js";
+import {exec, execSync} from "node:child_process";
+
+export async function publish(...flags){
+    const targets = await Target.readTargets(process.cwd(), flags);
+    for (let target of targets) {
+        try {
+            execSync("yarn npm publish", {
+                cwd: target.rootDir,
+                stdio: "ignore",
+            });
+            console.log(`publish ${target.packageJson.name}`)
+        }catch (e){
+            console.error(`Failed publish ${target.packageJson.name}\n${e.message}`)
+        }
+    }
+}
