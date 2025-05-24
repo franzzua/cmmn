7ed6b0d6bd8e635b38b93a19a2a53845fc702e89
Andrei Khamidulin <fransua.ne@gmail.com>
2025-05-24 21:12:03 +0200

    clean

---
+++ tools/commands/clean.ts
@@ -0,0 +1,13 @@
+import {getPackages} from "@manypkg/get-packages";
+import fs from "node:fs/promises";
+import {join} from "node:path";
+import {Flags} from "../helpers/flags";
+
+export async function clean(flags: Flags){
+    const packages = await getPackages(process.cwd())
+    for (const pkg of packages.packages) {
+        if (flags.workspace && flags.workspace != pkg.relativeDir)
+            continue;
+        await fs.rm(join(pkg.dir, 'dist'), { recursive: true });
+    }
+}
